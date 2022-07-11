245e23b7b119d9cbca74763d005849674f450b2a
Natcha Jintanasatien <natcha.jin@truedigital.com>
2022-07-11 18:08:23 +0700

    fix viewmodel

--- app/src/main/java/com/piglet/weather/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/piglet/weather/presentation/search/SearchViewModel.kt
@@ -1,14 +1,14 @@
 package com.piglet.weather.presentation.search
 
 import android.os.Bundle
-import com.piglet.foundation.base.ScopedViewModel
+import androidx.lifecycle.ViewModel
 import com.piglet.weather.navigation.Router
 import com.piglet.weather.navigation.router.SearchToWeather
 import com.piglet.weather.presentation.weather.WeatherFragment.Companion.KEY_EXTRA_LOCATION
 
 class SearchViewModel(
     private val router: Router
-) : ScopedViewModel() {
+) : ViewModel() {
 
     fun routeToWeather(keyword: String) {
         router.routeTo(
